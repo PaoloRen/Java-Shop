8e2cd448ed711e0a4edf8d162045a0c81d907a69
PaoloRen <pafly4542@gmail.com>
2024-11-21 18:22:09 +0100

    added:main

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,18 @@
+package org.lessons.java.shop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Prodotto libro = new Prodotto();
+        libro.nome = "Achille";
+        libro.prezzo = 10;
+        libro.iva = 22;
+        libro.descrizione = "libro";
+
+        System.out.println(libro.mostraIlPrezzo());
+        System.out.println(libro.prezzoComprensivoIva());
+        System.out.println(libro.nomeEsteso());
+    }
+
+
+}
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,25 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+    int codice = (int)(Math.random() * 101);
+    String nome;
+    String descrizione;
+    int prezzo;
+    int iva;
+
+
+
+//* Metodi di prodotto
+    public int mostraIlPrezzo(){
+        return prezzo;
+    }
+
+    public int prezzoComprensivoIva(){
+        return prezzo + (prezzo/100*iva);
+    }
+
+    public String nomeEsteso(){
+        return String.valueOf(codice)+"-"+nome;
+    }
 
 }
